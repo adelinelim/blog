80e254e65c12ffd3d0e69103ee3516e44c7cd5e3
adelinelim <adeline_yiing@hotmail.com>
2015-01-22 17:48:16 +0800

    test

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe User do
+RSpec.describe User do
 
   before(:each) do
     @attr = { email: "user@example.com", password: "something" }
   end
 
   it "should create a new instance given a valid attribute" do
-    user.create!(@attr)
+    User.create(@attr)
   end
 
 end
