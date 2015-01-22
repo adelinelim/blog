376b7297d0fd37ac6cfe7507104f95dcc913b0d0
adelinelim <adeline_yiing@hotmail.com>
2015-01-23 01:40:49 +0800

    heroku

--- db/migrate/20150116035741_create_posts.rb
+++ db/migrate/20150116035741_create_posts.rb
@@ -3,11 +3,11 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :content
-      t.integer :user_id
+      #t.integer :user_id
 
       t.timestamps
     end
-    add_index :posts, :user_id
+    #add_index :posts, :user_id
   end
 
   def self.down
